fdef6d5dd7e710d65da1abef453d35b47f7c500e
HanWonSeok <44897164+wonseok-han@users.noreply.github.com>
2022-10-18 13:05:22 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,13 @@
+function solution(dot) {
+    const x = dot[0];
+    const y = dot[1];
+    
+    if (x > 0) {
+        if (y > 0) return 1
+        else return 4
+    }
+    else {
+        if (y > 0) return 2
+        else return 3
+    }
+}
