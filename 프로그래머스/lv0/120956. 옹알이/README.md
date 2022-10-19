66de90edae2eb42690e376767135e8e5da1f721d
HanWonSeok <44897164+wonseok-han@users.noreply.github.com>
2022-10-19 17:24:15 +0900

    [level 0] Title: 옹알이, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이/README.md
+++ 프로그래머스/lv0/120956. 옹알이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옹알이 - 120956 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956#qna) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.59 ms
+메모리: 33.5 MB, 시간: 0.47 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120956. 옹알이/옹알이.js
+++ 프로그래머스/lv0/120956. 옹알이/옹알이.js
@@ -1,15 +1,14 @@
 const WORDS = ["aya", "ye", "woo", "ma"];
 
 function solution(babbling) {
-    const filtered = babbling.filter((word) => !new RegExp(WORDS.reduce((acc, cur) => [...acc, cur+cur], []).join("|"), "gi").test(word));
+    const doubleFiltered = babbling.filter((word) => !new RegExp(WORDS.reduce((acc, cur) => [...acc, cur+cur], []).join("|"), "gi").test(word));
     
-    const replaced = filtered.reduce((acc, cur) => {
+    const replaced = doubleFiltered.reduce((acc, cur) => {
         let replacedList = [];
         
         const regExp = new RegExp(WORDS.join("|"), "gi");
         if (regExp.test(cur)) {
-            const replacedStr = cur.replace(regExp, "");
-            replacedList.push(replacedStr);
+            replacedList.push(cur.replace(regExp, ""));
         }
 
         return [
