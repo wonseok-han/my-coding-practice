610d2665c66b7e547427f946ce658c8142b9eca2
HanWonSeok <44897164+wonseok-han@users.noreply.github.com>
2023-07-12 12:48:30 +0900

    [unrated] Title: 수 조작하기 1, Time: 5.55 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        
+        for (int i = 0; i < control.length(); i++) {
+            switch (String.valueOf(control.charAt(i))) {
+                case "w":
+                    answer += 1;
+                    break;
+                case "s":
+                    answer -= 1;
+                    break;
+                case "d":
+                    answer += 10;
+                    break;
+                case "a":
+                    answer -= 10;
+                    break;
+                default:
+                    break;
+            }            
+        }
+        
+        return answer;
+    }
+}
