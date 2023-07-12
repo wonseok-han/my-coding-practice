f5a582452700fbe5ec6bbaaa70331007bb54d013
HanWonSeok <44897164+wonseok-han@users.noreply.github.com>
2023-07-12 16:03:22 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] != arr[j][i]) {
+                    return answer = 0;
+                }
+            }
+        }
+
+        
+        return answer;
+    }
+}
